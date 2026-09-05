b132e4438335a2b26aa862c8eee6adb188d90bb7
ajam1t <amit01071994@gmail.com>
2026-09-05 23:21:33 +0530

    Delete apps/web directory

--- apps/web/src/app/(auth)/layout.tsx
+++
@@ -1,7 +0,0 @@
-export default function AuthLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <div className="min-h-screen bg-paper flex flex-col items-center justify-center px-4 py-12">
-      {children}
-    </div>
-  )
-}
